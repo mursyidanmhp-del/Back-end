d8882fa68e3f7411f05c0a9a70a29625ad19ee1f
nazmisani <nazminouval@gmail.com>
2026-07-31 09:46:22 +0700

    spread operator

---
+++ destructuring-spread-async/destructuring.js
@@ -0,0 +1,13 @@
+let siswa = {
+    nama: "Abdul",
+    kelas: 3,
+    umur: 17
+}
+
+// let nama = siswa.nama
+// let kelas = siswa.kelas
+
+const { nama, umur } = siswa
+
+
+console.log(nama + umur);
